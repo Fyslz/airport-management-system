67f40feb07de87c08e6cc6ac16012b4f4e08b591
AhmedS1-AI <hamoodesameer5@gmail.com>
2026-05-13 01:11:01 +0300

    done servicrUnit

--- src/models/ServiceUnit.java
+++ src/models/ServiceUnit.java
@@ -1,5 +1,55 @@
 package models;
 
-public class ServiceUnit {
-    
+public abstract class ServiceUnit {
+    private int unitId;
+    private double serviceDuration;
+    private static int availableServiceUnits;
+    private boolean isAvailable;
+    private double cost;
+    private int currentGate;
+
+    public ServiceUnit(int unitId, double serviceDuration, double cost) {
+        this.unitId = unitId;
+        this.serviceDuration = serviceDuration;
+        this.cost = cost;
+        this.isAvailable = true;
+        this.currentGate = -1;
+        availableServiceUnits++;
+    }
+
+    public abstract void provideService(Flight f);
+
+    public void moveToGate(int gateId) {
+        this.currentGate = gateId;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        this.isAvailable = available;
+        if (available) availableServiceUnits++;
+        else availableServiceUnits--;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public double getServiceDuration() {
+        return serviceDuration;
+    }
+
+    public int getUnitId() {
+        return unitId;
+    }
+
+    public int getCurrentGate() {
+        return currentGate;
+    }
+
+    public static int getAvailableServiceUnits() {
+        return availableServiceUnits;
+    }
 }
